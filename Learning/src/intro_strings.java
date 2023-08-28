2066252b8b2e233861ce38dbc097142648cb095f
Samir <143437931+samirdesu@users.noreply.github.com>
2023-08-28 23:27:13 +0530

    Add files via upload

---
+++ Learning/src/intro_strings.java
@@ -0,0 +1,52 @@
+import java.sql.SQLOutput;
+
+public class intro_strings {
+    public static void main(String[] args) {
+/*        String name = "Be cold";
+        System.out.println(name);
+          String name = new String("No place for good guy, Be a maniac ");
+        System.out.println(name);
+        int a = 10; // %d for int
+        float b = 20.6f; // %f for float
+        String na= "Samir";// %s for string
+        char d = 'S'; //%c for char
+        System.out.printf("Hi this is %s and total value of a is %d " +
+                "and total value of b is %.2f and value of d is %c", na,a,b,d);
+        // %.2f will reduce the numbers after decimal and
+        // %.2f will reduce the numbers after decimal and %3.2f will create 3 spaces before the float.
+
+// */
+//        String name = "Kiran";
+//        int value=name.length();
+//        System.out.println(value);
+//        System.out.println(name.substring(2));
+//        System.out.println(name.substring(1,4));
+//        System.out.println(name.replace("a","Sa"));
+//        System.out.println(name.startsWith("kir"));// case sensetive
+//        System.out.println(name.endsWith("n"));
+//        System.out.println(name.charAt(2));
+//        System.out.println(name.equals("Kiraan"));
+//        System.out.println(name.equalsIgnoreCase("kIRaaN"));
+
+        String modiname = "Khanrsamirn";
+        System.out.println(modiname.indexOf("n"));
+        System.out.println(modiname.indexOf("i",6));
+        System.out.println(modiname.lastIndexOf("r"));
+        System.out.println(modiname.lastIndexOf("r",10));
+        System.out.println("There is no place \n for kindness");
+        System.out.println("Dont be \t Kind");
+        System.out.println("No one want\'kind\'people in this wolrd");
+        System.out.println("Be a complete \n maniac");
+
+
+
+//        String nontrim = "   Samir   ";
+//        System.out.println(nontrim);
+//        System.out.println(nontrim.trim());
+//        System.out.println(nontrim.toUpperCase());
+//        System.out.println(nontrim.toLowerCase());
+
+
+
+    }
+}
